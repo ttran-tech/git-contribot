3e3cdedc9c522585a9e4b6dd8df73935d716caa3
Tan <duytantran2710@gmail.com>
2025-03-07 20:50:52 -0800

    Test: add unit test

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+
+# Brief:        This file contains the unit tests for git-contribot project.
+#
+# Repository:   https://github.com/ttran-tech/git-contribot.git
+# Author:       ttran.tech
+# Email:        duy@ttran.tech
+# Github:       https://github.com/ttran-tech
+# 
+
+class TestGitContribot(unittest.TestCase):
+    def test_is_repo_exist(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
