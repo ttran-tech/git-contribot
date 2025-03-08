af18b1f1ca22112041397a699ffe8b21ea61cea6
Tan <duytantran2710@gmail.com>
2025-03-07 22:21:33 -0800

    Refactor: change functio name to indicate its scope on remote repo

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ from main import *
 
 class TestGitContribot(unittest.TestCase):
     def test_is_repo_exist(self):
-        self.assertEqual(is_repo_exist(TEST_REPO_URL), True, "Result mismatch") # Test for pass result
-        self.assertEqual(is_repo_exist(DUMMY_REPO_URL), False, "Result mismatch") # Test for fail result
+        self.assertEqual(is_remote_repo_exist(TEST_REPO_URL), True, "Result mismatch") # Test for pass result
+        self.assertEqual(is_remote_repo_exist(DUMMY_REPO_URL), False, "Result mismatch") # Test for fail result
 
     def test_repo_name(self):
         self.assertIsNotNone(extract_repo_name(TEST_REPO_URL))
