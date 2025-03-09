df33136407d5cffd03be5675bd507d2ab8a17f41
Tan <duytantran2710@gmail.com>
2025-03-09 14:21:05 -0700

    Feat: call cleanup_repo after make commit

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ Email:        duy@ttran.tech
 Github:       https://github.com/ttran-tech
 """
 from .common import print_banner, print_separator
-from .repo import is_remote_repo_exist, clone_repo
+from .repo import is_remote_repo_exist, clone_repo, cleanup_repo
 from .core import make_commit_concurrent
 from .config import get_user_config
 
@@ -18,11 +18,13 @@ def main():
 
     repo_url = user_config['repo-url']
     repo_name = user_config['repo-name']
+    local_repo_path = user_config['local-repo-path']
     print_separator()
     if is_remote_repo_exist(repo_url):
         clone_repo(repo_url, repo_name)
         make_commit_concurrent(user_config)
-        print("\n => Finished.")
+        cleanup_repo(local_repo_path)
+        print("\n → Finished.")
 
 
 # if __name__ == '__main__':
